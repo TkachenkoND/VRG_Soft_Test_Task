5ddbb16342732db9feb672eb60a08bb1478e2afa
TkachenkoND00 <tkachenkond00@gmail.com>
2022-05-20 21:12:39 +0300

    create data module di

--- app/src/main/java/com/example/vrg_soft_test_task/di/DataModule.kt
+++ app/src/main/java/com/example/vrg_soft_test_task/di/DataModule.kt
@@ -1,2 +1,9 @@
 package com.example.vrg_soft_test_task.di
 
+import com.example.vrg_soft_test_task.data.repository_impl.TopPublicationRepositoryImpl
+import com.example.vrg_soft_test_task.domain.repository.TopPublicationRepository
+import org.koin.dsl.module
+
+val dataModule = module {
+    single<TopPublicationRepository> { TopPublicationRepositoryImpl(get()) }
+}
