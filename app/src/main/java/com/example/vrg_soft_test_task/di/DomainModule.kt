91b79245d56379c49fbf04d5d0c9cee09bc0d603
TkachenkoND00 <tkachenkond00@gmail.com>
2022-05-20 21:14:10 +0300

    create domain module di

--- app/src/main/java/com/example/vrg_soft_test_task/di/DomainModule.kt
+++ app/src/main/java/com/example/vrg_soft_test_task/di/DomainModule.kt
@@ -1,2 +1,8 @@
 package com.example.vrg_soft_test_task.di
 
+import com.example.vrg_soft_test_task.domain.usecase.LoadTopPublicationUseCase
+import org.koin.dsl.module
+
+val domainModule = module {
+    factory { LoadTopPublicationUseCase(get()) }
+}
