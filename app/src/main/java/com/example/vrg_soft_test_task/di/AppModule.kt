143f12fce619a9c42e2975a149a591db73edbe1f
TkachenkoND00 <tkachenkond00@gmail.com>
2022-05-20 21:20:18 +0300

    create app module di

--- app/src/main/java/com/example/vrg_soft_test_task/di/AppModule.kt
+++ app/src/main/java/com/example/vrg_soft_test_task/di/AppModule.kt
@@ -1,2 +1,9 @@
 package com.example.vrg_soft_test_task.di
 
+import com.example.vrg_soft_test_task.presentation.view_model.TopPublicationViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel { TopPublicationViewModel(get()) }
+}
