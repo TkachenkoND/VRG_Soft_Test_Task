9eb7d4d223dfee115f5b913eaf6d3937507aa2b8
TkachenkoND00 <tkachenkond00@gmail.com>
2022-05-20 21:27:09 +0300

    made additions to view model

--- app/src/main/java/com/example/vrg_soft_test_task/presentation/view_model/TopPublicationViewModel.kt
+++ app/src/main/java/com/example/vrg_soft_test_task/presentation/view_model/TopPublicationViewModel.kt
@@ -1,10 +1,33 @@
 package com.example.vrg_soft_test_task.presentation.view_model
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.vrg_soft_test_task.domain.models.TopPublicationModel
 import com.example.vrg_soft_test_task.domain.usecase.LoadTopPublicationUseCase
+import kotlinx.coroutines.launch
 
 class TopPublicationViewModel(
     private val loadTopPublicationUseCase: LoadTopPublicationUseCase,
 ) : ViewModel() {
 
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _topPublication = MutableLiveData<TopPublicationModel>()
+    val topPublication: LiveData<TopPublicationModel> = _topPublication
+
+    fun loadTopPublicationVm() {
+        viewModelScope.launch {
+            try {
+                _topPublication.postValue(loadTopPublicationUseCase.loadTopPublication())
+                _isLoading.postValue(true)
+            } catch (e: Exception) {
+                Log.d("LoadException", e.toString())
+                _isLoading.postValue(false)
+            }
+        }
+    }
 }
