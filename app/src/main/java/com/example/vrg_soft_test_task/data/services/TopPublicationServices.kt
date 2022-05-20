f4462c70e6b331abe539a1c2d0248ecef20a83a2
TkachenkoND00 <tkachenkond00@gmail.com>
2022-05-20 21:04:25 +0300

    create top publication services

--- app/src/main/java/com/example/vrg_soft_test_task/data/services/TopPublicationServices.kt
+++ app/src/main/java/com/example/vrg_soft_test_task/data/services/TopPublicationServices.kt
@@ -1,4 +1,9 @@
 package com.example.vrg_soft_test_task.data.services
 
+import com.example.vrg_soft_test_task.domain.models.TopPublicationModel
+import retrofit2.http.GET
+
 interface TopPublicationServices {
+    @GET("/json")
+    suspend fun getTopPublication(): TopPublicationModel
 }
