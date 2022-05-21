4a95139f3e9a58cf022d155618714dbc3eff7b2b
TkachenkoND00 <tkachenkond00@gmail.com>
2022-05-21 23:07:11 +0300

    add change in dialog

--- app/src/main/java/com/example/vrg_soft_test_task/presentation/view/fragment/PublicationsFragment.kt
+++ app/src/main/java/com/example/vrg_soft_test_task/presentation/view/fragment/PublicationsFragment.kt
@@ -3,7 +3,6 @@ package com.example.vrg_soft_test_task.presentation.view.fragment
 import android.content.ContentResolver
 import android.content.ContentValues
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
@@ -12,21 +11,29 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.vrg_soft_test_task.R
 import com.example.vrg_soft_test_task.databinding.PublicationsFragmentBinding
 import com.example.vrg_soft_test_task.presentation.view.adapter.ClickOnTheImg
 import com.example.vrg_soft_test_task.presentation.view.adapter.ClickOnTheSaveImg
 import com.example.vrg_soft_test_task.presentation.view.adapter.TopPublicationRvAdapter
+import com.example.vrg_soft_test_task.presentation.view.dialog.ClickPositiveButton
 import com.example.vrg_soft_test_task.presentation.view.dialog.SaveDialog
 import com.example.vrg_soft_test_task.presentation.view_model.TopPublicationViewModel
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 const val IMAGES_FOLDER_NAME = "gg"
 
-class PublicationsFragment : Fragment(), ClickOnTheImg, ClickOnTheSaveImg {
+class PublicationsFragment : Fragment(), ClickOnTheImg, ClickOnTheSaveImg, ClickPositiveButton {
 
     private var _binding: PublicationsFragmentBinding? = null
     private val binding get() = _binding!!
@@ -86,11 +93,72 @@ class PublicationsFragment : Fragment(), ClickOnTheImg, ClickOnTheSaveImg {
     }
 
     override fun saveImgPress(imgUrl: String) {
-        SaveDialog(imgUrl).show(childFragmentManager, SaveDialog.TAG)
+        SaveDialog(imgUrl,this@PublicationsFragment as ClickPositiveButton).show(childFragmentManager, SaveDialog.TAG)
+    }
+
+    override fun click(imgUrl: String) {
+        convertUrlToBitmapGlide(imgUrl)
+    }
+
+    private fun saveImage(bitmap: Bitmap) {
+        val name = "gg"
+
+        val fos: OutputStream? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val resolver: ContentResolver = requireContext().contentResolver
+            val contentValues = ContentValues()
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, "DCIM/$IMAGES_FOLDER_NAME")
+            val imageUri =
+                resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            resolver.openOutputStream(imageUri!!)
+        } else {
+            val imagesDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM
+            ).toString() + File.separator + IMAGES_FOLDER_NAME
+            val file = File(imagesDir)
+            if (!file.exists()) {
+                file.mkdir()
+            }
+            val image = File(imagesDir, "$name.png")
+            FileOutputStream(image)
+        }
+        val isSave = bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
+        fos!!.flush()
+        fos.close()
+    }
+
+    private fun convertUrlToBitmapGlide(imgUrl: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(imgUrl)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onResourceReady(
+                    resource: Bitmap,
+                    transition: Transition<in Bitmap>?
+                ) {
+                    saveImage(resource)
+                    Toast.makeText(
+                        requireContext(),
+                        "Успішно збережено до галереї)",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+
+                override fun onLoadFailed(errorDrawable: Drawable?) {
+                    super.onLoadFailed(errorDrawable)
+                    Toast.makeText(requireContext(), "gg", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
